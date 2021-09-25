e05f0a1c9a4fc0beb6499501d1626f7a4d1eb36c
dgstjohn <dgstjohn@gmail.com>
2021-09-24 21:58:49 -0400

    routes, models, server and dateFormat all set

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+
+// for /api/thoughts
+// GET to get all thoughts
+// POST to create a new thought
+// (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router.route('/').get(getThoughts).post(createThought);
+
+// GET to get a single thought by its _id
+// PUT to update a thought by its _id
+// DELETE to remove a thought by its _id
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// for /api/thoughts/:thoughtId/reactions
+// POST to create a reaction stored in a single thought's reactions array field
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+// for /api/users
+// GET all users
+// POST new user
+router.route('/').get(getUsers).post(createUser)
+
+// GET single user by its _id and populated thought and friend data
+// PUT to update user by its _id
+// DELETE to remove user by its id
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser)
+
+// for /api/users/:userId/friends/:friendId
+// POST to add a new friend to a user's friend list
+// DELETE to remove a friend from a user's friend list
+router.route(':userId/friends/:friendId').post(addFriend).delete(deleteUser);
+
+// BONUS: remove user's associated thoughts when deleted
+
+module.exports = router;
